1105c79bbbcd51c1eafe6e1050b49f8b9d8ef9c2
valentinacamacho <valentinacamacho0206@gmail.com>
2025-04-24 17:21:27 -0500

    App checks if there is a token in the localStorage. If there is, it shows the Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React,{ useState,useEffect, use } from "react";
 import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const [autenticado, setAutenticado] = useState(false);
+
+  useEffect(()=>{
+    const token = localStorage.getItem("token");
+    if(token){
+      setAutenticado(true);
+    }
+  })
   return (
     <div className="App">
-    <Login/>
+      {autenticado ? <Dashboard/> : <Login/>}
     </div>
   );
 }
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div>
+      <h2>Bienvenida</h2>
+      <p>Has iniciado sesión correctamente</p>
+    </div>
+  )
+}
+
+export default Dashboard;
